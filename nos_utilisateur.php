621b2d809be262418f292e0fbe5a8921f4ec9066
nassimipssi <n.barkallah@ecole-ipssi.net>
2022-02-03 20:18:19 +0100

    debut POO

--- nos_utilisateur.php
+++ nos_utilisateur.php
@@ -1,7 +1,8 @@
 <?php
-require_once("../db.php");
-require_once("../login/user_info.php");
-require_once("../class/user.php");
+require_once "db.php";
+require_once "login/user_info.php";
+require_once "class/user.php";
+require_once "info_panier.php";
 ?>
 <!DOCTYPE html>
 <html lang="FR">
@@ -9,23 +10,25 @@ require_once("../class/user.php");
 <head>
     <meta charset="UTF-8">
     <title>Panier</title>
-    <link rel="stylesheet" type="text/css" href="../CSS/basket.css?v=<?= ver() ?>" />
-    <link rel="icon" type="image/png" href="../Image/HeadLogo.png" />
-    <link rel="stylesheet" type="text/css" href="../CSS/HomePage.css?v=<?= ver() ?>" />
+    <link rel="stylesheet" type="text/css" href="CSS/basket.css?v=<?= ver() ?>" />
+    <link rel="icon" type="image/png" href="Image/HeadLogo.png" />
+    <link rel="stylesheet" type="text/css" href="CSS/HomePage.css?v=<?= ver() ?>" />
 </head>
 
 <body>
     <?php
     getUserInfo();
+    getPanierInfo();
     if ($USER_INFO->getID() >= 1) {
-        require_once("../header.php");
+        require_once("header.php");
+        
     ?>
         <div class="contenu">
 
 
         </div>
     <?php
-        require_once("../footer.php");
+        require_once("footer.php");
     } else {
     ?>
         <div class="info_noConnect">
